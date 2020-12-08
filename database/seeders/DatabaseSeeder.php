732cf51213e7f447ebeba9e57e9fbeda984cad0e
KHaritov Andrei <avxodiar@gmail.com>
2020-12-09 02:29:38 +0300

    Посев данных

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            PagesSeeder::class,
+            PeoplesSeeder::class,
+            PortfoliosSeeder::class,
+            ServicesSeeder::class
+        ]);
     }
 }
---
+++ database/seeders/PortfoliosSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PortfoliosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('portfolios')->insert([
+            [
+                'name' => 'SMS MOBILE APP',
+                'image' => 'portfolio_pic1.jpg',
+                'filter' => 'APPLE IOS'
+            ],
+            [
+                'name' => 'Finance App',
+                'image' => 'portfolio_pic2.jpg',
+                'filter' => 'DESIGN'
+            ],
+            [
+                'name' => 'GPS Concept',
+                'image' => 'portfolio_pic3.jpg',
+                'filter' => 'DESIGN'
+            ],
+            [
+                'name' => 'Shopping',
+                'image' => 'portfolio_pic4.jpg',
+                'filter' => 'PROTOTYPE,ANDROID,WEB APP'
+            ],
+            [
+                'name' => 'Managment',
+                'image' => 'portfolio_pic5.jpg',
+                'filter' => 'DESIGN'
+            ],
+            [
+                'name' => 'iPhone',
+                'image' => 'portfolio_pic6.jpg',
+                'filter' => 'APPLE IOS,WEB APP'
+            ],
+            [
+                'name' => 'Nexus Phone',
+                'image' => 'portfolio_pic7.jpg',
+                'filter' => 'DESIGN,WEB APP'
+            ],
+            [
+                'name' => 'Android',
+                'image' => 'portfolio_pic8.jpg',
+                'filter' => 'ANDROID'
+            ]
+        ]);
+    }
+}
